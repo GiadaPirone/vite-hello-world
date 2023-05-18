d4749985acd5120bf282e8528c2bc8a3cb83f2da
GiadaPirone <123646621+GiadaPirone@users.noreply.github.com>
2023-05-18 16:36:31 +0200

    prova

---
+++ my-vue-app/src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import AppHeader from './components/AppHeader.vue'
+
+export default {
+  components: {
+    AppHeader,
+    
+  }
+}
+</script>
+
+
+<template>
+ <header>
+  <AppHeader />
+ </header>
+</template>
+
+<style scoped></style>
---
+++ my-vue-app/src/components/AppHeader.vue
@@ -0,0 +1,10 @@
+<script>
+
+export default {
+    name: "AppHeader",
+}
+</script>
+
+<template>
+    <h1>"La mia prima app con Vite!"</h1>
+</template>
